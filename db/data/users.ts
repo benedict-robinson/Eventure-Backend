836d11c2302d001404f8e8134fa3e5d2d7d283a7
Benedict Robinson <benedict.r1713@gmail.com>
2025-04-16 17:57:06 +0100

    Partial data and seed setup

---
+++ db/data/users.ts
@@ -0,0 +1,21 @@
+const users = [
+    {
+      username: "user1",
+      email: "user1@example.com",
+      is_staff: true,
+      image_url: "https://avatar.iran.liara.run/public/boy?username=Ash",
+      favourites: [3, 6, 1],
+      going: [3, 2],
+      owned_events: [3] 
+    },
+    {
+        username: "user2",
+        email: "user2@example.com",
+        is_staff: false,
+        image_url: "https://avatar.iran.liara.run/public/boy?username=Ash",
+        favourites: [3, 6, 1],
+        going: [3, 2],
+        owned_events: [] 
+      },
+    { username: "user3", is_staff: false }
+  ];
