e25e8c3d9e293f413b60e8c546752a96d82f37ec
Benedict Robinson <benedict.r1713@gmail.com>
2025-04-18 18:43:01 +0100

    Sorted by City

--- server/controllers/events-controllers/get-events.ts
+++ server/controllers/events-controllers/get-events.ts
@@ -1,9 +1,11 @@
 import { Request, Response, NextFunction } from "express";
-import { selectEvents } from "../../models/events-models/select-events"
+import { selectEvents } from "../../models/events-models/select-events";
 import { EventInterface } from "../../../db/data/events";
 
 export const getEvents = (req: Request, res: Response, next: NextFunction) => {
-    selectEvents().then((events: EventInterface[]) => {
-        res.status(200).send({ events });
-    })
-}
+  const city = (req.query.city as string) || "";
+
+  selectEvents(city).then((events: EventInterface[]) => {
+    res.status(200).send({ events });
+  });
+};
--- server/ticketmaster-requests/get-events-request.ts
+++ server/ticketmaster-requests/get-events-request.ts
@@ -10,36 +10,49 @@ interface GetEventsRequest {
   params: {
     apikey: any;
     size: number;
+    city?: string
   };
 }
 
-const getEvents: GetEventsRequest = {
-  method: "GET",
-  url: `https://app.ticketmaster.com/discovery/v2/events/`,
-  params: {
-    apikey: apiKey,
-    size: 50,
-  },
-};
-
-export const getTicketmasterEvents = () => {
-  return axios(getEvents).then(({ data: { _embedded: { events } } }) => {
-    const trimmedEvents = events.map((e: any) => {
-        return {
+export const getTicketmasterEvents = (city: string) => {
+  const getEventsObj: GetEventsRequest = {
+    method: "GET",
+    url: `https://app.ticketmaster.com/discovery/v2/events/`,
+    params: {
+      apikey: apiKey,
+      size: 50
+    },
+  };
+  if (city) {
+    getEventsObj.params.city = city
+  }
+  return axios(getEventsObj)
+    .then(
+      ({
+        data: {
+          _embedded: { events },
+        },
+      }) => {
+        const trimmedEvents = events.map((e: any) => {
+          return {
             name: e.name,
             api_event_id: e.id,
             url: e.url,
             img: formatImageObj(e.images[0]),
             date_and_time: formatDateAndTimeObj(e.dates),
-            tags: [e.classifications[0].segment.name, e.classifications[0].genre.name],
-            info: e.info,
-            location: e.place || {location: "No Location Provided"},
-            description: e.promoter?.description || "",
-        }
-    })
-    return trimmedEvents
-  })
-  .catch((err) => {
-    console.log(err)
-  })
+            tags: e.classifications ? [
+              e.classifications[0].segment.name,
+              e.classifications[0].genre.name,
+            ] : [],
+            info: e.info || "",
+            location: e.place || {city: city }|| { location: "No Location Provided" },
+            description: e.description || e?.promoter?.description || "",
+          };
+        });
+        return trimmedEvents;
+      }
+    )
+    .catch((err) => {
+      console.log(err);
+    });
 };
