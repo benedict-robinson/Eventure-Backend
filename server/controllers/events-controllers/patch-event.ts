cdb2e389bd3f646324d1aa919d8b038c893bbf5c
Benedict Robinson <benedict.r1713@gmail.com>
2025-04-21 19:00:04 +0100

    Patch Events & Error Testing

---
+++ server/controllers/events-controllers/patch-event.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from "express";
+import { updateEvent } from "../../models/events-models/update-event";
+import { EventInterface } from "../../../db/data/events";
+
+export const patchEvents = (req: Request, res: Response, next: NextFunction) => {
+    const { username } = req.params
+    const newEvent = req.body
+    if (!newEvent.event_id) {
+        return Promise.reject({status: 400, msg: "Bad Request - No Event Specified"})
+    }
+    updateEvent(username, newEvent).then((response: EventInterface) => {
+        res.status(200).send({event: response})
+    })
+    .catch((err) => {
+        next(err)
+    })
+}
--- server/routes/events-router.ts
+++ server/routes/events-router.ts
@@ -1,10 +1,12 @@
 import express, { Router, Request, Response } from "express";
 import { getEvents } from "../controllers/events-controllers/get-events";
 import { postEvent } from "../controllers/events-controllers/post-events";
+import { patchEvents } from "../controllers/events-controllers/patch-event";
 
 const eventsRouter: Router = express.Router();
 
 eventsRouter.get("/", getEvents)
 eventsRouter.post("/:username", postEvent)
+eventsRouter.patch("/:username", patchEvents)
 
 export { eventsRouter };
