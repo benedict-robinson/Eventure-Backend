6210bd761b384fc2bea07bbf6afe0b72c33e8b11
Benedict Robinson <benedict.r1713@gmail.com>
2025-04-23 16:17:10 +0100

    Set Up for Hosting

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,12 @@
 const { Pool } = require("pg");
-const ENV = /*process.env.NODE_ENV ||*/ "development";
+const ENV = process.env.NODE_ENV || "development";
 
 require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error("PG Database not set");
+  throw new Error("Database not set");
 }
 
 
---
+++ db/seed/run-seed.ts
@@ -0,0 +1,4 @@
+import { users, events } from "../data";
+import { seed } from "./seed";
+
+seed(users, events)
