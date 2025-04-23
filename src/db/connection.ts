a943243a3c3f31ca0c312df601741d420be73628
Benedict Robinson <benedict.r1713@gmail.com>
2025-04-23 19:13:12 +0100

    connection to .ts

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -11,7 +11,7 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
 
 
 
-const config = {};
+const config: any = {};
 
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
